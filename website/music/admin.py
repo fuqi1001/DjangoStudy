f583d762b4b464d0e7a9d8c26c90f6c95f7b737b
qifu <fuqiqi12345@gmail.com>
2018-01-16 09:28:36 -0500

    add detail for album

--- website/music/admin.py
+++ website/music/admin.py
@@ -3,5 +3,10 @@ from .models import Album, Song
 
 # Register your models here.
 
+'''
+creating admin account: python3 manage.py createsuperuser
+
+'''
+
 admin.site.register(Album)
 admin.site.register(Song)
