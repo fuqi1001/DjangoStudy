cc5e8d29497608c1f8c75e3df173674851ea735e
qifu <fuqiqi12345@gmail.com>
2018-01-16 09:59:28 -0500

    render

--- website/music/views.py
+++ website/music/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Album
-from django.template import loader
+from django.shortcuts import render
+# from django.template import loader [first way to render template]
+
 # Create your views here.
 
 def index(request):
@@ -12,11 +14,11 @@ def index(request):
         url = '/music/' + str(album.id) +'/'
         html += '<a href="'+ url +'">'+ album.album_title +'</a><br>'
     '''
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums' : all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    # template = loader.get_template('music/index.html') [first way to render template]    
+    # return HttpResponse(template.render(context, request)) [first way]
+    
+    context = {'all_albums' : all_albums}
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
